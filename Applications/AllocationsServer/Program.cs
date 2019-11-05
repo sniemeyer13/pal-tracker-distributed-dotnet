sumankum26/pal-tracker-distributed

Add config server

Applications/AllocationsServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Pivotal.Extensions.Configuration.ConfigServer;
 
 namespace AllocationsServer
 {
@@ -20,7 +20,7 @@ namespace AllocationsServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

Applications/BacklogServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Pivotal.Extensions.Configuration.ConfigServer;
 
 namespace BacklogServer
 {
@@ -20,7 +20,7 @@ namespace BacklogServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

Applications/RegistrationServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Pivotal.Extensions.Configuration.ConfigServer;
 
 namespace RegistrationServer
 {
@@ -20,7 +20,7 @@ namespace RegistrationServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

Applications/TimesheetsServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Pivotal.Extensions.Configuration.ConfigServer;
 
 namespace TimesheetsServer
 {
@@ -20,7 +20,7 @@ namespace TimesheetsServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

sniemeyer13/pal-tracker-distributed-dotnet

Applications/AllocationsServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 
 namespace AllocationsServer
 {
@@ -20,7 +20,7 @@ namespace AllocationsServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

Applications/BacklogServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 
 namespace BacklogServer
 {
@@ -20,7 +20,7 @@ namespace BacklogServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

Applications/RegistrationServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 
 namespace RegistrationServer
 {
@@ -20,7 +20,7 @@ namespace RegistrationServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }

Applications/TimesheetsServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 
 namespace TimesheetsServer
 {
@@ -20,7 +20,7 @@ namespace TimesheetsServer
                 // https://github.com/aspnet/KestrelHttpServer/issues/1998#issuecomment-322922164
                 .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
                 .UseCloudFoundryHosting()
-                .AddCloudFoundry()
+                .AddConfigServer()
                 .UseStartup<Startup>();
     }
 }